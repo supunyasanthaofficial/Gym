f60589306ffa4a669e2be7c4a9150676133540ec
supunyasantha <supunyasantha.official@gmail.com>
2025-12-08 16:29:48 +0530

    changes fontsize and color in galarry section text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -150,7 +150,7 @@ const AboutPage = () => {
           </div>
 
           <div className="text-center mt-12">
-            <p className="text-gray-600 font-medium">
+            <p className="text-black font-bold">
               More than just a gym - it's a lifestyle destination
             </p>
           </div>
