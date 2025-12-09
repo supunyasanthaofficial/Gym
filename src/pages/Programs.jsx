298f329d82c87985bd4fcf72b5a817dd1345e0fc
supunyasantha <supunyasantha.official@gmail.com>
2025-12-09 10:25:00 +0530

    change the font size

--- src/pages/Programs.jsx
+++ src/pages/Programs.jsx
@@ -17,7 +17,7 @@ const Programs = () => {
           Explore Our Programs
         </h1>
 
-        <p className="text-center text-xl text-black mb-10 font-bold">
+        <p className="text-center text-xl text-black mb-10 font-semibold">
           Find the perfect fitness program to match your goals and get started
           on your journey today.
         </p>
